9872446c7406e085ef2d68ab9a98d1cafbd8c280
IliaBelou <ilia.belou@gmail.com>
2021-07-07 01:37:38 +0300

    Lesson9

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"./configReader"
+	"fmt"
+	"errors"
+)
+
+var GLOBAL_ConfigParsingErr = errors.New("Config data validation error")
+
+
+func main()  {
+	var cfg,err = configReader.NewConfig("./conf.yaml")
+
+	if !err {
+		fmt.Println(cfg)
+	}else{
+		errPars := fmt.Errorf("config pars error in main: %w", GLOBAL_ConfigParsingErr)
+		fmt.Println(errPars.Error())
+	}
+
+}
